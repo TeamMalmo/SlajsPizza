ac4210c7e6e64d87049a25b3953c0ecdcdaa5ad0
Caroline Edgren <caroline.edgren88@gmail.com>
2024-11-22 10:31:49 +0100

    Created MainComponent

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,41 @@
+<!-- SCRIPTS -->
+<script setup>
+
+</script>
+
+<!-- TEMPLATE -->
+<template>
+<header>
+  <h1>Pizza Slajs</h1>
+</header>
+
+<main>
+  <p>Welcome to Pizza Slajs</p>
+</main>
+
+<footer>
+  <p>Made with 💩 by Malmö</p>
+</footer>
+
+</template>
+
+<!-- STYLE -->
+<style scoped>
+header {
+  background-color: skyblue;
+  color: black;
+  text-align: center;
+}
+
+main {
+  background-color: white;
+  color: black;
+  text-align: center;
+}
+
+footer {
+  background-color: skyblue;
+  color: black;
+  text-align: center;
+}
+</style>
