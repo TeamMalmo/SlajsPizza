398846a2bfddb879e6cdb0d87323f0aee99b6681
Caroline Edgren <caroline.edgren88@gmail.com>
2024-11-22 12:15:54 +0100

    Added dumydata

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,29 +1,52 @@
 <!-- SCRIPTS -->
+
+
 <script setup>
 import { useMenu } from '../composables/useMenu.js';
 
 const { menu, isLoading, error, fetchMenu } = useMenu();
 
-
 console.log(menu.value);
+
+const dummyData = { // Changed from "dummmyData" to "dummyData"
+  id: 1,
+  type: "pizza",
+  name: "Romana",
+  imgUrl: "https://pizzerialunden.com/wp-content/themes/pizzerialunden/images/default-landingpage-1.jpg",
+  description: "Pizza ipsum dolor amet platter mayo pesto, thin crust bianca bacon & tomato...",
+  price: 129,
+  toppings: ["mozarella", "parmaskinka", "oliver"],
+};
 </script>
 
+
 <!-- TEMPLATE -->
-<template>
+
+    <template>
 <header>
   <h1>Pizza Slajs</h1>
 </header>
 
 <main>
   <p>Welcome to Pizza Slajs</p>
-
+  <div class="card">
+    <img :src="dummyData.imgUrl" alt="Pizza Image" class="card-image" />
+    <div class="card-content">
+      <h2 class="card-title">{{ dummyData.name }}</h2>
+      <p class="card-description">{{ dummyData.description }}</p>
+      <ul class="card-toppings">
+        <li v-for="topping in dummyData.toppings" :key="topping">{{ topping }}</li>
+      </ul>
+      <p class="card-price">{{ dummyData.price }} SEK</p>
+    </div>
+  </div>
 </main>
 
 <footer>
   <p>Made with 💩 by Malmö</p>
 </footer>
-
 </template>
+
 
 <!-- STYLE -->
 <style scoped>
@@ -43,5 +66,11 @@ footer {
   background-color: skyblue;
   color: black;
   text-align: center;
+}
+
+img
+{
+  width: 300px;
+  height: auto;
 }
 </style>
