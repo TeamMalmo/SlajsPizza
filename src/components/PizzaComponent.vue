d3d7d2fb466162d4f7088fe075206eafeff0785e
Eva Björling <eva.bjorling@chasacademy.se>
2024-11-22 12:44:59 +0100

    menu toggles + basic styling

--- src/components/PizzaComponent.vue
+++ src/components/PizzaComponent.vue
@@ -1,40 +1,63 @@
 <!-- SCRIPTS -->
 <script setup>
-// Computed property to filter pizzas
-import { computed } from 'vue';
-import {useMenu} from '../composables/useMenu.js';
+import { computed, ref, onMounted } from 'vue';
+import { useMenu } from '../composables/useMenu.js';
 
+// destructure allt från vår composable
 const { menu, isLoading, error, fetchMenu } = useMenu();
+
+// filterar pizzorna ur menyn
 const pizzas = computed(() => menu.value.filter(item => item.type === 'pizza'));
+
+// enkelt state för att visa menyn
+const isMenuShowing = ref(false);
+
+// toggle funktionen
+const toggleMenu = () => {
+  isMenuShowing.value = !isMenuShowing.value;
+};
+
+// hämtar menyn när komponenten startas
+onMounted(async () => {
+  await fetchMenu();
+});
 </script>
+
 
 <!-- TEMPLATE -->
 <template>
-<div class="pizza-container">
-  <h1>Pizza</h1>
-  <button @click="fetchMenu" :disabled="isLoading">Show Our Pizzas</button>
-  <div v-if="isLoading">Loading...</div>
-  <div v-if="error" class="error">{{ error }}</div>
-  <div v-if="pizzas.length">
-    <div v-for="item in pizzas" :key="item.id" class="pizza-item">
-        <div class="left">
-            <h2>{{ item.name }}</h2>
-            <img :src="item.imgUrl" :alt="item.name" />
+    <div class="pizza-container">
+      <h1>Pizza</h1>
+      <!-- toogle pizza meny -->
+      <button @click="toggleMenu" :disabled="isLoading">{{ isMenuShowing ? 'Hide Pizzas ⬆️' : 'Show Pizzas ⬇️' }}</button>
+  
+      <!-- renderar laddning eller error före menyn visas -->
+      <div v-if="isLoading">Loading...</div>
+      <div v-if="error" class="error">{{ error }}</div>
+    
+      <!-- meny visas om menyn är påslaget -->
+      <div class="rendered-menu" v-show="isMenuShowing">
+        <!-- loopar över alla pizzorna i menyn -->
+        <div v-for="item in pizzas" :key="item.id" class="pizza-item">
+          <div class="left">
+                <h2>{{ item.name }}</h2>
+                <img :src="item.imgUrl" :alt="item.name" />
+            </div>
+            <div class="right">
+                <h3>Price: {{ item.price }}:-</h3>
+                <p>{{ item.description }}</p>
+           <!-- inre loop över toppings eller ingredienser -->
+            <ul v-if="item.toppings || item.ingredients">
+              <li v-for="topping in item.toppings || item.ingredients" :key="topping">
+                {{ topping }}
+              </li>
+            </ul>
+          </div>
         </div>
-        <div class="right">
-        <p>{{ item.description }}</p>
-        <p>Price: {{ item.price }}</p>
-        <ul v-if="item.toppings || item.ingredients">
-            <li v-for="topping in item.toppings || item.ingredients" :key="topping">
-            {{ topping }}
-            </li>
-        </ul>
-        </div>
+      </div>
     </div>
-  </div>
-</div>
-
-</template>
+  </template>
+  
 
 <!-- STYLE -->
 <style scoped>
@@ -43,20 +66,68 @@ const pizzas = computed(() => menu.value.filter(item => item.type === 'pizza'));
       flex-direction: column;
       justify-content: center;
       align-items: center;
-      gap : 10px;
-      min-height: 100vh;
+      gap : 1rem;
+      /* min-height: 100vh; */
+      font-family: Verdana, Geneva, Tahoma, sans-serif;
     }
 
+    button {
+        padding: 1rem;
+        border: skyblue solid 3px;
+        border-radius: 5px;
+        color: skyblue;
+        text-transform: uppercase;
+        background-color: white;
+    }
+    
+
+    .rendered-menu {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      gap : 10px;
+    }
+
+    h2, h3 {
+      text-align: center;
+      margin: 0;
+      color: white;
+    }
     .pizza-item {
       display: flex;
       /* flex-direction: column; */
       justify-content: space-between;
       align-items: center;
       max-width: 500px;
+      border: skyblue solid 3px;
+      border-radius: 1rem;
+      padding: 1rem;
+      background-color: skyblue;
+      gap: 1rem;
     }
 
     .pizza-item img {
       width: 200px;
       height: 200px;
+    }
+
+    .right, .left {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: center;
+        gap : 1rem;
+        text-align: left;
+        height: 100%;
+    }
+
+    .right {
+        max-width: 250px;
+    }
+
+    .pizza-item ul li {
+        list-style:circle;
+        text-align: left;
     }
 </style>
